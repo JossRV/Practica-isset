ff8426b1aef0a0df43c0a5568175deeac4a433ad
JossRV <jossrv74@gmail.com>
2022-02-24 12:47:54 -0600

    Practica de calucladora con isset

---
+++ app/dependencias.php
@@ -0,0 +1,9 @@
+<!-- Dep estilo -->
+<link rel="stylesheet" href="packages/css/bootstrap5.css">
+<link rel="stylesheet" href="packages/css/sweetalert2.css">
+
+
+<!-- Dep js -->
+<script src="packages/js/jquery-3.6.0.min.js"></script>
+<script src="packages/js/bootstrap5.js"></script>
+<script src="packages/js/sweetalert2.js"></script>
